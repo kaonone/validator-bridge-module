51b84d9a2f71220676a3cb47047be6c80cd83273
Andrei Orlov <aaorlov1@gmail.com>
2019-11-25 21:16:23 +0400

    remove ethereum_event_listener

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ use std::sync::mpsc::channel;
 mod config;
 mod controller;
 mod controller_storage;
-mod ethereum_event_listener;
 mod ethereum_transactions;
 mod executor;
 mod graph_node_event_listener;
@@ -24,8 +23,6 @@ fn main() {
 
     let controller_thread = controller::spawn(config.clone(), controller_rx, executor_tx);
     let executor_thread = executor::spawn(config.clone(), executor_rx);
-    let ethereum_event_listener_thread =
-        ethereum_event_listener::spawn(config.clone(), controller_tx.clone());
     let graph_node_event_listener_thread =
         graph_node_event_listener::spawn(config.clone(), controller_tx.clone());
     let substrate_event_listener_thread =
@@ -33,7 +30,6 @@ fn main() {
 
     let _ = controller_thread.join();
     let _ = executor_thread.join();
-    let _ = ethereum_event_listener_thread.join();
     let _ = graph_node_event_listener_thread.join();
     let _ = substrate_event_listener_thread.join();
 }
