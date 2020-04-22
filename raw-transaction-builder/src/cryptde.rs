03418a2c960eea167f5c2af3c7bfe72c3bd8123f
Aleks Gnatovskyi <aleks.work2222@gmail.com>
2020-04-22 17:15:31 +0300

    shut warnings for now

--- raw-transaction-builder/src/cryptde.rs
+++ raw-transaction-builder/src/cryptde.rs
@@ -1,4 +1,6 @@
 // Copyright (c) 2017-2019, Substratum LLC (https://substratum.net) and/or its affiliates. All rights reserved.
+#![allow(unused)]
+
 use base64;
 use ethsign_crypto::Keccak256;
 use rustc_hex::ToHex;
